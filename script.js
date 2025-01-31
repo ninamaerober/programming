8dee57bc7927ccbc99029dfc1c55fc2ccbed1082
ninamaerober <ninamaerober.202300768@gmail.com>
2025-01-31 17:23:34 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// Variables
+let name = "John";
+let age = 25;
+let isStudent = true;
+console.log(`Name: ${name}, Age: ${age}, Student: ${isStudent}`);
+// Loops
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
+// Functions
+function addNumbers(a, b) {
+  return a + b;
+}
+console.log(addNumbers(5, 10));
+// Conditionals
+if (age >= 18) {
+  console.log("Adult");
+} else {
+  console.log("Minor");
+}
